292eb5bb335bc4789b5faa188e8b2aff7f80f5f8
Duy-DaoNg <duy.dao0604@gmail.com>
2024-02-22 10:13:09 +0700

    hide AIO key

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from simple_ai import *
 
 AIO_FEED_ID = ["nutnhan1", "nutnhan2"]
 AIO_USERNAME = "duydao0604"
-AIO_KEY = 
+AIO_KEY = ""
 
 def connected(client):
     print("Ket noi thanh cong ...")
